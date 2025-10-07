4a3041a6300c244e108b08e20b76b9cbc163b0da
Matthew Romano <matbat2205@gmail.com>
2025-10-06 20:37:34 -0400

    Added functionality

--- MoodToMusicTranslator/MoodToMusicTranslator.py
+++ MoodToMusicTranslator/MoodToMusicTranslator.py
@@ -1,14 +1,13 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 from textblob import TextBlob
 
 blob = TextBlob("I feel amazing!");
 print(blob.sentiment)
 
-
-
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
     client_id = "5f05092321d0497fb3242888b76e7d3d",
     client_secret ="ffabe06efcdf4f7b89d5b4755c1afd7b",
 ))
+results = sp.search(q="chill playlist", type="playlist", limit=1)
+print(results['playlists']['items'][0]['name'])
