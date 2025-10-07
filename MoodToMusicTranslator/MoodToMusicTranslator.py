eba76c918289eb3be9d4517095de590d1ec7cb53
Matthew Romano <matbat2205@gmail.com>
2025-10-06 20:29:45 -0400

    Updated read me and added requirements.txt

--- MoodToMusicTranslator/MoodToMusicTranslator.py
+++ MoodToMusicTranslator/MoodToMusicTranslator.py
@@ -1,1 +1,7 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
+    client_id = "",
+    client_secret ="",
+))
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # MoodToMusicTranslator
+For setup and installation run "pip install -r requirements.txt" in the main "MoodToMusicTransalter" folder.
