e2d1d9f91dcc7748340ed60ad9c6d1bed52bc419
Matthew Romano <matbat2205@gmail.com>
2025-10-06 20:35:25 -0400

    Added credentials

--- MoodToMusicTranslator/MoodToMusicTranslator.py
+++ MoodToMusicTranslator/MoodToMusicTranslator.py
@@ -1,6 +1,7 @@
 from textblob import TextBlob
 
-
+blob = TextBlob("I feel amazing!");
+print(blob.sentiment)
 
 
 
@@ -8,6 +9,6 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
-    client_id = "",
-    client_secret ="",
+    client_id = "5f05092321d0497fb3242888b76e7d3d",
+    client_secret ="ffabe06efcdf4f7b89d5b4755c1afd7b",
 ))
